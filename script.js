1f6131e939faf4b332641d9843702029edda4e85
Joao Amestica <joao.amestica@gmail.com>
2024-08-10 11:27:41 -0400

    inicio proyecto

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    console.log('cargando DOM...');
+
+    let posts = [];
+    let paginaActual = 1;
+    let cantidadPostPorPagina = 5;
+
+    function fetchPosts(){
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(posts => posts.json())
+        .then( postsJson => {
+            posts = postsJson;
+            console.log('Posts dentro del array: ', posts)
+            refrescoPosts();
+        })
+        .catch( error => console.error('Error en fetch', error))    
+    }
+
+    function refrescoPosts(){
+        const inicio = (paginaActual - 1) * cantidadPostPorPagina;
+        const final = inicio + cantidadPostPorPagina;
+        const postsActuales = posts.slice(inicio, final);
+
+        //....
+    }
+
+    // inicio de mi aplicacion
+    fetchPosts();
+
+})
